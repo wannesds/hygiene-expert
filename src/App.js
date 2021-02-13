594bbb5b134d616b57353cb92e0b6daf541bf548
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-02-13 15:51:43 +0100

    added home screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="lang-select-box">
+          <a href=''>NL</a>
+          <a href=''>FR</a>
+          <a href=''>ENG</a>
+        </div>
+
     </div>
   );
 }
